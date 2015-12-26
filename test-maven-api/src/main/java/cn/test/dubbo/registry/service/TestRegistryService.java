ee71f51020df75718f4069129a643a8c09ef8ac8
lixiong2009 <lixiong2009@163.com>
2015-12-26 13:58:26 +0800

    Initial commit

---
+++ test-maven-api/src/main/java/cn/test/dubbo/registry/service/TestRegistryService.java
@@ -0,0 +1,13 @@
+package cn.test.dubbo.registry.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LX
+ * Date: 2015/12/26 12:57
+ * Desc:
+ */
+public interface TestRegistryService {
+
+    public String hello(String name);
+
+}
